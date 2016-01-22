a8083be7961e5a0e23eafc01a7e68325af57d409
gqguan <gqguan@hotmail.com>
2016-01-22 15:33:28 +0800

    Generate the new dcmd.dll file

--- etc/autocomp.bat
+++ etc/autocomp.bat
@@ -16,22 +16,26 @@ echo Processing toolkits.f90    ... [02/10]: toolkits subroutine
 call aspcomp toolkits.f90
 echo Processing mdvar.f         ... [03/10]: AspenPlus configuration variables definition
 call aspcomp mdvar.f
-echo Processing d1mach.f        ... [04/10]: Subroutine for machine number, invoked by integal solver
+echo Processing newton_interp_1d.f90 and pwl_interp_1d.f90
+echo                            ... [04/10]: Interpolation subroutines
+call aspcomp newton_interp_1d.f90
+call aspcomp pwl_interp_1d.f90
+echo Processing d1mach.f        ... [05/10]: Subroutine for machine number, invoked by integal solver
 call aspcomp d1mach.f
 call aspcomp i1mach.f 
-echo Processing dqk15.f         ... [05/10]: Integal solver
+echo Processing dqk15.f         ... [06/10]: Integal solver
 call aspcomp dqk15.f
-echo Processing diff.f          ... [06/10]: Deriviation solver
+echo Processing diff.f          ... [07/10]: Deriviation solver
 call aspcomp diff.f
-echo Processing property.f90    ... [07/10]: Subroutines of physical properties
+echo Processing property.f90    ... [08/10]: Subroutines of physical properties
 call aspcomp property.f90
-echo Processing localflux.f90   ... [08/10]: Subroutine for local heat and mass transfer rates
+echo Processing localflux.f90   ... [09/10]: Subroutine for local heat and mass transfer rates
 call aspcomp localflux.f90
-echo Processing CalcProf.f90    ... [09/10]: Subroutine for get the temperature profiles
+echo Processing CalcProf.f90    ... [10/10]: Subroutine for get the temperature profiles
 call aspcomp CalcProf.f90
-echo Processing CalcSOUT.f90    ... [10/10]: Main program
+echo Processing CalcSOUT.f90    ... [11/10]: Main program
 call aspcomp CalcSOUT.f90
-echo Process dcmd.f             ... [11/10]: Aspen interface program
+echo Process dcmd.f             ... [12/10]: Aspen interface program
 call aspcomp dcmd.f
 echo.
 echo Compilation finished ...
